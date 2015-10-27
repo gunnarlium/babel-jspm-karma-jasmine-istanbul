2ae02065fedac777fc1feb02c1926077b96a9640
Eduard Jacko <jacko.eduard@gmail.com>
2015-10-27 14:03:39 +0000

    upgraded dependencies jspm         ^0.15.7  →  ^0.16.13 karma       ^0.12.37  →  ^0.13.14 karma-jspm    ^1.1.5  →    ^2.0.2

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,9 @@ module.exports = function (config) {
 		},
 
 		proxies: {
-			'/base': '/base/src'
+			// '/base': '/base/src'
+			'/src/': '/base/src',
+			'/jspm_packages/': '/src/jspm_packages'
 		},
 
 		browsers: ['PhantomJS'],
