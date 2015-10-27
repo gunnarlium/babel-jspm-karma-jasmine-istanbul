11652d23e1423733c14ae6d1090b7822fc1db11c
Eduard Jacko <jacko.eduard@gmail.com>
2015-10-27 14:08:25 +0000

    fixed normalized path warning for proxies in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -22,9 +22,8 @@ module.exports = function (config) {
 		},
 
 		proxies: {
-			// '/base': '/base/src'
-			'/src/': '/base/src',
-			'/jspm_packages/': '/src/jspm_packages'
+			'/src/': '/base/src/',
+			'/jspm_packages/': '/src/jspm_packages/'
 		},
 
 		browsers: ['PhantomJS'],
